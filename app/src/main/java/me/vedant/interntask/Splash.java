e79d7b9e9c9208074cd5724520c43b7e799adfdf
6vedant@gmail.com <6vedant@gmail.com>
2018-11-22 12:06:26 +0530

    Splash Updated

--- app/src/main/java/me/vedant/interntask/Splash.java
+++ app/src/main/java/me/vedant/interntask/Splash.java
@@ -2,13 +2,29 @@ package me.vedant.interntask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class Splash extends AppCompatActivity {
+
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        //here fetch the data from firebase database
 
 
     }
