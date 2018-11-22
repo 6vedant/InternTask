aea0cfaabd915f62a0761941684062a069f2ff64
6vedant@gmail.com <6vedant@gmail.com>
2018-11-22 11:19:22 +0530

    Tab4 Adapter  Fixed

--- app/src/main/java/me/vedant/interntask/fragment/Tab4.java
+++ app/src/main/java/me/vedant/interntask/fragment/Tab4.java
@@ -2,6 +2,7 @@ package me.vedant.interntask.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +12,10 @@ import android.widget.ProgressBar;
 import java.util.ArrayList;
 
 import me.vedant.interntask.R;
+import me.vedant.interntask.adapter.Tab2Adapter;
+import me.vedant.interntask.adapter.Tab4Adapter;
 import me.vedant.interntask.prototype.Tab1Prototype;
+import me.vedant.interntask.prototype.Tab4Prototype;
 
 
 /**
@@ -43,20 +47,41 @@ public class Tab4 extends Fragment {
 
         ArrayList<Tab1Prototype> tab1Prototypes = (ArrayList<Tab1Prototype>) getArguments().getSerializable("tab1prototypes");
 
-        progressBar = (ProgressBar)view.findViewById(R.id.progressbar);
-        recyclerView = (RecyclerView)view.findViewById(R.id.recyclerview);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressbar);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
 
         ArrayList<String> series_name = new ArrayList<>();
         series_name.clear();
 
-        for(Tab1Prototype tab1Prototype : tab1Prototypes){
+        ArrayList<Tab1Prototype> tab1Prototypes1 = new ArrayList<>();
+        ArrayList<Tab4Prototype> tab4Prototypes = new ArrayList<>();
+        tab4Prototypes.clear();
 
-            if(!series_name.contains(tab1Prototype.getSeries_name())){
+        for (Tab1Prototype tab1Prototype : tab1Prototypes) {
+
+            if (!series_name.contains(tab1Prototype.getSeries_name())) {
                 series_name.add(tab1Prototype.getSeries_name());
             }
         }
 
+        for (int i = 0; i < series_name.size(); i++) {
+            tab1Prototypes1.clear();
+            for (Tab1Prototype tab1Prototype : tab1Prototypes) {
+                if (tab1Prototype.getSeries_name().equals(series_name.get(i))) {
+                    tab1Prototypes1.add(tab1Prototype);
+                }
+            }
+            tab4Prototypes.add(new Tab4Prototype(series_name.get(i), tab1Prototypes1));
 
+        }
+
+        if (tab4Prototypes.size() > 0) {
+            Tab4Adapter tab4Adapter = new Tab4Adapter(getContext(), tab4Prototypes);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(linearLayoutManager);
+            recyclerView.setAdapter(tab4Adapter);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
 
 
         return view;
